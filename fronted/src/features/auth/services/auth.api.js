fac2644c676f5a827eb6577b09505215c1b9d4b3
Meenakshi <meenakshichhonker@gmail.com>
2026-05-24 23:29:27 +0530

    bug fixed

--- fronted/src/features/auth/services/auth.api.js
+++ fronted/src/features/auth/services/auth.api.js
@@ -3,7 +3,6 @@ import axios from "axios"
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
-    withCredentials: true
 })
 
 api.interceptors.request.use((config) => {
